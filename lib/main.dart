dbbf212e98aedfc42ae54512dfcb3bd2be0cdb6e
Yusuf Orhan <yazilimcigenc2@gmail.com>
2023-11-02 14:52:17 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: false,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home Page")),
+      body: Center(
+        child: Column(
+          children: [
+            const Text("Hello World"),
+            ElevatedButton(
+                onPressed: () {
+
+                },
+                child: const Text("Goto Second Screen"))
+          ],
+        ),
+      ),
+    );
+  }
+}
